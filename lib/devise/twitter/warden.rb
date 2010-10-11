56a2def70d186041ede565ad6867f495710c4741
maxdemarzi <maxdemarzi@hotmail.com>
2010-10-10 17:04:20 -0700

    added user call back

--- lib/devise/twitter/warden.rb
+++ lib/devise/twitter/warden.rb
@@ -3,9 +3,10 @@ Warden::OAuth.access_token_user_finder(:twitter) do |access_token|
   twitter_handle = access_token.params[:screen_name]
   klass = @env['devise.mapping'].class_name.constantize
 
+  already_existing_user = klass.find_by_twitter_handle(twitter_handle)
+
   if perform_connect
     # Add twitter_handle to current user
-    already_existing_user = klass.find_by_twitter_handle(twitter_handle)
     if already_existing_user.blank?
       # We don't know anyone with this handle, therefore continue with connecting
       user = @env['warden'].user
@@ -34,6 +35,7 @@ Warden::OAuth.access_token_user_finder(:twitter) do |access_token|
       user.twitter_handle = twitter_handle
       user.twitter_oauth_token = access_token.token
       user.twitter_oauth_secret = access_token.secret
+      user.before_twitter_auto_create(already_existing_user, twitter_handle)
       user.save
     end
 
